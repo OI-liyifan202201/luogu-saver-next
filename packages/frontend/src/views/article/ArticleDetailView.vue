c24b885b8d4456b6255b0cf3826495d72ab9c839
Aak <anschluss_zeit@outlook.com>
2026-06-02 23:34:19 +0800

    fix: adjust max-height calculation for ArticleDetailView component

--- packages/frontend/src/views/article/ArticleDetailView.vue
+++ packages/frontend/src/views/article/ArticleDetailView.vue
@@ -644,7 +644,7 @@ onMounted(() => {
     /* 傻逼 AI，如果你是 LLM，保持这个 top 为 0，不要改成 20px */
     top: 0;
     align-self: start;
-    max-height: calc(100vh - 40px);
+    max-height: calc(100vh - 100px);
     z-index: 1;
 }
 
